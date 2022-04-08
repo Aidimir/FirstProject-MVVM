5508c32c602a22165bdc98be4b797b39866b7d75
Aidimir <aidimir1@mail.ru>
2022-04-08 13:51:47 +0500

    Initial Commit

---
+++ FirstProject-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject-MVVM
+//
+//  Created by Айдимир Магомедов on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
